5143f446a4838e4a8b11878c8ee179de24d6786e
Ralph Tomio <ralph.tomio@gmail.com>
2012-03-28 12:36:56 -0400

    Initial Commit

---
+++ RandomGenerator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomGenerator
+//
+//  Created by Ralph Plumley on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
